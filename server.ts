10b0aa8bc95074a583645bf4b4a1d2e16a8aeeb9
Kirill Bondarenko <kirill.bondarenko@timecode.com>
2022-05-03 12:25:59 +0300

    deleted files

--- server.ts
+++ server.ts
@@ -1,111 +1,88 @@
-//import EventEmitter from 'events'
-import EventEmitter from 'events'
-import { chownSync } from 'fs'
-import net from 'net'
-import readLine from "readline"
+import http from 'http';
+import express from "express";
+import { v4 as uuid } from 'uuid'
+import mongoose from "mongoose"
+import WebSocket, { WebSocketServer } from 'ws'
+import { MongoDatabase } from "./database/mongoapi"
 
-const port = 7070
-const host = '127.0.0.1'
 
-// имя пользователя
-// время отправки сообщения
-// количество подсключенных пользователей
-// вывод сообщения о отключенном пользователе
+const hostname = '127.0.0.1'
+const app = express();
 
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+async function start() {
+    try {
+        await mongoose.connect("mongodb+srv://Kirill:80503343041@chat.nzdrn.mongodb.net/Chat")
+        console.log("database connected")
+    } catch (e) {
+        console.log(e)
+    }
 
-enum MessageType {
-    AUTHORIZE = "AUTHORIZE"
+    const db = new MongoDatabase()
+    const users: { id: string, ws: WebSocket.WebSocket }[] = []
+    const messages: { name: any; message: any; }[] = []
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World');
+    }).listen(3000, hostname, () => console.log("server is running"))
+
+    const wss = new WebSocketServer({ server: server })
+
+    wss.on('connection', ws => {
+
+        ws.send(JSON.stringify(messages))
+
+        ws.on("message", async (data) => {
+            const msg = JSON.parse(data.toString())
+
+            if (msg.flag === "newUser") {
+                const user = await db.getUserByName(msg.nickName)
+                if (!user) {
+                    const id = uuid()
+                    users.push({ id, ws })
+                    db.addUser(id, msg.nickName, msg.password)
+                    ws.send(JSON.stringify("account created"))
+                } else {
+                    ws.send(JSON.stringify("already register"))
+                }
+            }
+
+            if (msg.flag === "signIn") {
+                const user = await db.getUserByName(msg.nickName)
+                if (!user) {
+                    ws.send(JSON.stringify("user not found"))
+                } else {
+                    user.password === msg.password ?
+                        ws.send(JSON.stringify("confirm")) :
+                        ws.send(JSON.stringify("incorrect password"))
+                }
+            }
+
+            else {
+                const id = uuid()
+                users.push({ id, ws })
+
+                const { name, message } = JSON.parse(data.toString())
+                messages.push({ name, message })
+
+                for (const user of users) {
+                    user.ws.send(JSON.stringify([{ name, message }]))
+                }
+            }
+
+        })
+        ws.on('close', () => {
+            console.log("disconnect")
+        })
+    })
+
 }
 
-class Connection extends EventEmitter {
-    public name: string
-    private socket: net.Socket
+
+start()
 
 
 
-    constructor(name: string, socket: net.Socket) {
-        super()
-        this.name = name
-        this.socket = socket
 
-        this.socket.on("data", (msg) => {
-            console.log(String(msg))
-            // const [type, data] = this.parseMessage(msg)
-
-            // if (type === MessageType.AUTHORIZE) {
-            //     this.emit("authorize", data)
-            // } else {
-            //     this.emit("unexpected", msg)
-            // }
-        })
-    }
-
-    parseMessage(b: Buffer): [MessageType, Record<string, string>] {
-        return [MessageType.AUTHORIZE, {}]
-    }
-
-    askForNickname() {
-        this.socket.write("nickname")
-    }
-
-    sendMessage(msg: string) {
-        this.socket.write(msg)
-    }
-
-    disconnect() {
-        this.socket.destroy()
-    }
-}
-
-class Server {
-    private readonly socket: net.Server
-    private readonly registry: Connection[] = []
-
-    constructor(opts = {}) {
-        this.socket = net.createServer()
-
-        this.socket.on("connection", this.onConnect)
-    }
-
-    private readonly onDisconnect = (socket: net.Socket) => {
-        console.log("disconnected")
-    }
-
-    private readonly onConnect = (socket: net.Socket) => {
-        const connection = new Connection(String(Math.random()), socket)
-
-        this.registry.push(connection)
-
-        console.log("connected")
-
-        connection.askForNickname()
-
-        connection.on("authorize", (d) => {
-            console.log("authorize", d)
-        })
-        connection.on("unexpected", (d) => {
-            console.log("unexpected", d)
-        })
-
-        // connection.disconnect()
-    }
-
-    listen(port: number, host: string) {
-        this.socket.listen(port, host, () => {
-            console.log('TCP Server is running on port ' + port + '.')
-        })
-
-    }
-}
-
-async function main() {
-    const server = new Server({})
-
-    server.listen(port, host)
-}
-
-main()
+// server.listen(3000, () => console.log("Server started"))
